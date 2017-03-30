20a88b76942385449be7b2533d38f0d613a6de31
mrodriguez-kcg <mark1@kochcreativegroup.com>
2017-03-30 10:49:27 -0500

    Checkpoint on Master Sheet

--- dist/assets/js/src/assets/js/app.js
+++ dist/assets/js/src/assets/js/app.js
@@ -1,5 +1,6 @@
 $(document).foundation();
 
+//Navigation show/hide
 var scrolled;
 var ScrollTop = 0;
 var delta = 5;
@@ -13,46 +14,40 @@ setInterval(function() {
         scrolled = !1
     }
 }, 250);
-
 function hasScrolled() {
     var st = $(this).scrollTop();//ST = curent position of scroll   NAV DOWN IS DARK
     if (Math.abs(ScrollTop - st) <= delta)//if 0 - curent location is less than or =to 5 return
         return;
     if (st > ScrollTop && st > navHeight) {//if current scroll is larger than 0 and it is greater than nav height remove the class nav down and add nav up
         $('header').removeClass('nav-down').addClass('nav-up');
-        // Turn this back on for svg animation ->
-        //$('.logo-path').removeClass('on').removeClass('on-quick').addClass('off');
     } else if (st + $(window).height() < $(document).height()) {
-        $('header').removeClass('nav-up').addClass('nav-down');
-        //$('.logo-path').removeClass('nav-down');
-        //$('.logo-path.on').removeClass('on');
-        
+        $('header').removeClass('nav-up').addClass('nav-down');        
         if (st == 0) {
             $('header').removeClass('nav-down');
-            //console.log('remove nav down');
-            //$('.logo-path.on').addClass('on');
-           // Turn this back on for svg animation ->
-           // $('.logo-path').removeClass('off').addClass('on-quick');
         }
     } 
         
     ScrollTop = st;
-    console.log(st)
+    // console.log(st)
 }
+//End Navigation show/hide
 
-
+//Mobile Nav Script
+//Targets mobile nav toggle
 $( ".burger" ).click(function() {
   $( this ).toggleClass("open").toggleClass("close");
   $(".navigation-column:last-child .row").toggleClass("open");
   $('header').toggleClass("lock");
   console.log("nav-opened");
 });
-// $(".has-sub").click(function() {
-//     $(this).find(".mobile-sub-nav").addClass("sub-open").removeClass("sub-close");
-//     console.log("sub-opened");
-// });
-// $(".mobile-nav-back").click(function(e) {
-//     e.stopPropagation();
-//     $(this).parent(".mobile-sub-nav.sub-open").removeClass("sub-open").addClass("sub-close");
-//     console.log("sub-closed");
-// });
+//Sub nav for mobile
+$(".has-sub").click(function() {
+    $(this).find(".mobile-sub-nav").addClass("sub-open").removeClass("sub-close");
+    console.log("sub-opened");
+});
+$(".mobile-nav-back").click(function(e) {
+    e.stopPropagation();
+    $(this).parent(".mobile-sub-nav.sub-open").removeClass("sub-open").addClass("sub-close");
+    console.log("sub-closed");
+});
+//End Mobile Nav Script
